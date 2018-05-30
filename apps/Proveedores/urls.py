e456f4d742ea36839d69a24088d67539f697c801
marcos.borja <mtbguason@hotmail.com>
2018-05-30 15:01:08 -0600

    upd proveedores

--- apps/Proveedores/urls.py
+++ apps/Proveedores/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from apps.Proveedores import views
 
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
-    path ( '' , views.ProveedorList.as_view() , name = 'proveedores.index' ),
-    path ( 'nuevo' , views.ProveedorCreate.as_view() , name = 'proveedores.nuevo' ),
-    path ( 'actualizar/<pk>' , views.ProveedorActualizar.as_view() , name = 'proveedores.actualizar' ),  
-    path ( 'detalle/<pk>' , views.ProveedorDetalles.as_view() , name = 'proveedores.detalles' ),  
-    path ( 'eliminar/<pk>' , views.ProveedorEliminar.as_view() , name = 'proveedores.eliminar' ),
+    path ( '' , login_required(views.ProveedorList.as_view()) , name = 'proveedores.index' ),
+    path ( 'nuevo' , login_required(views.ProveedorCreate.as_view()) , name = 'proveedores.nuevo' ),
+    path ( 'actualizar/<pk>' , login_required(views.ProveedorActualizar.as_view()) , name = 'proveedores.actualizar' ),  
+    path ( 'detalle/<pk>' , login_required(views.ProveedorDetalles.as_view()) , name = 'proveedores.detalles' ),  
+    path ( 'eliminar/<pk>' , login_required(views.ProveedorEliminar.as_view()) , name = 'proveedores.eliminar' ),
 ]
